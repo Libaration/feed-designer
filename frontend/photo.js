e298b3db3d664c3c832446756fc69e54bb0b6a9e
Cristian Deleon <libaration@gmail.com>
2021-02-10 14:46:58 -0500

    append photos to ul as an li for each photo object passed into displayPhoto

--- frontend/photo.js
+++ frontend/photo.js
@@ -6,13 +6,16 @@ class Photo {
     }
 
     displayPhoto() {
-        const photoContainer = document.getElementById('photoContainer')
-        const photo = document.createElement('img')
-        photo.src = this.url;
-        const caption = document.createElement('h2')
+        const photoList = document.querySelector('ul.photos')
+        const photo = document.createElement('li')
+        photo.id = `photo${this.id}`
+        const photoImage = document.createElement('img')
+        photoImage.src = this.url;
+        const caption = document.createElement('h3')
         caption.innerText = this.caption
-        photoContainer.appendChild(photo)
-        photoContainer.appendChild(caption)
+        photo.appendChild(photoImage)
+        photo.appendChild(caption)
+        photoList.appendChild(photo)
         
     }
 }
